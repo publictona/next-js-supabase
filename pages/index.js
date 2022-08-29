1c8568f0472861ea69d9cc53865836040312228e
publictona <landgesushma2@gmail.com>
2022-08-29 11:04:14 +0530

    env

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 import styles from '../styles/Home.module.css'
 import React from 'react'
+//
+//import {supabase} from './utils/supabase'
 
- const index = () => {
-  return (
-    <div>index
-    <h1>hello sushma</h1>
-    </div>
-  )
+export async function getStaticProps() {
+  return {
+    props: {
+      user : [],
+    },
+  };
 }
 
-export default index
 
+ function Home({ user }) {
+  //console.log(supabase.auth.user());
+
+  return (
+    <div className={styles.container}>
+      <h1>Hello user!</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </div>
+  );
+}
+
+export default Home
