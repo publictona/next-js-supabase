2cf7af8a90fbb2d4256fb558187b5b3aed023bfe
publictona <landgesushma2@gmail.com>
2022-09-19 17:25:47 +0530

    only table completed on supabase

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,43 @@
 import styles from '../styles/Home.module.css'
 import React from 'react'
-//
+import { createClient } from '@supabase/supabase-js'
 //import {supabase} from './utils/supabase'
 
+
+
 export async function getStaticProps() {
-  return {
-    props: {
-      user : [],
-    },
-  };
+
+
+const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+  process.env.SUPABASE_API_KEY || ''
+)
+
+const { data} = await supabaseAdmin
+.from('user')
+.select('*')
+.user('id');
+
+return {
+  props: {
+    user: data,
+  },
+};
 }
+// function cn({...classes: "string"}){
+//   return classes.filter(Boolean).join(' ')
+// }
 
 
- function Home({ user }) {
-  //console.log(supabase.auth.user());
+// function Home({ user }:{user:user[]})
+//  return (
+//     <div className={styles.container}>
+//       <h1>Hello user!</h1>
 
-  return (
-    <div className={styles.container}>
-      <h1>Hello user!</h1>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-    </div>
-  );
-}
+//       {user.map((user) =>(userkey={user.id} user={user}))}
+//       <pre>{JSON.stringify(user, null, 2)}</pre>
+//     </div>
+//   );
+// }
 
 export default Home
